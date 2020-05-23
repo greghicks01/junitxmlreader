aa1927785427f256bc169ad22dd249710c3dec2d
greg <greghicks@iinet.net.au>
2020-05-23 21:41:37 +1000

    [XML] PXJR-1 - junitxml - Reads files or throws errors for anything that is not a valid XML

---
+++ libs/xmlreader/tests/test_open_xml.py
@@ -0,0 +1,32 @@
+"""
+    junit_xml Tests
+"""
+
+import pytest
+import xml.dom.minidom
+
+from libs.xmlreader.xml_reader.junitxml import XmlFileParser
+
+
+@pytest.mark.parametrize('filename', ['./data/output.xml', './data/multisuite.xml'])
+def test_open_good_xml(filename):
+
+    junit_xml = XmlFileParser(filename)
+
+    assert junit_xml is not None
+    assert type(junit_xml) is XmlFileParser
+
+
+@pytest.mark.parametrize('filename', ['./data/empty.xml', './data/malformed.xml'])
+def test_open_bad_xml(filename):
+
+    with pytest.raises(xml.parsers.expat.ExpatError):
+        junit_xml = XmlFileParser(filename)
+
+
+
+@pytest.mark.parametrize('filename', 'nonexistant.xml')
+def test_open_filenotfound(filename):
+
+    with pytest.raises(FileNotFoundError):
+        junit_xml = XmlFileParser(filename)
