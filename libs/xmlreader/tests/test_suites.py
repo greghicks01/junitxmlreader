53a8ab561fb173d90e09e3fc181614e155a87ccc
greg <greghicks@iinet.net.au>
2020-05-23 21:51:01 +1000

    [SUITE] PJXR-2 - testsuite - Iterator of all test suite level elements

---
+++ libs/xmlreader/tests/test_suites.py
@@ -0,0 +1,35 @@
+"""
+ testsuites with one and two elements
+"""
+
+import pytest
+from xml.dom.minidom import Element
+from venv.libs.xmlreader.xmlreader.junitxml import XmlFileParser
+from venv.libs.xmlreader.xmlreader.suites import Suites
+
+
+@pytest.mark.parametrize('filename,expected_count', [('./data/output.xml', 1), ('./data/multisuite.xml', 2)])
+def test_suite_count(filename, expected_count):
+
+    junit_xml = XmlFileParser(filename)
+
+    suites = Suites(junit_xml.xml_doc)
+
+    assert suites is not None
+
+    suite_count = 0
+    while suites.next():
+        suite_count += 1
+
+    assert suite_count == expected_count
+
+
+def test_suite():
+
+    junit_xml = XmlFileParser('./data/output.xml')
+
+    suites = Suites(junit_xml.xml_doc)
+
+    suites.next()
+
+    assert type(suites.get_current()) is Element
