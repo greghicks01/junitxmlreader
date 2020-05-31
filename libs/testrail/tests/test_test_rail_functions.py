10fe105022af4b46fded9fb6bd4c14e01b59b359
greg <greghicks@iinet.net.au>
2020-05-31 13:48:42 +1000

    [FEATURE] PJXR-6 - testrail class - TestRail Reporting feature

--- libs/testrail/tests/test_test_rail_functions.py
+++ libs/testrail/tests/test_test_rail_functions.py
@@ -2,8 +2,8 @@ from libs.testrail.testrail.testrail_functions import TestRailFunctions
 
 
 url = 'https://greg595.testrail.io/'
-name = 'greg595au@gmail.com'
-key = 't7ewUHFUt2MtO46O.b27-Cxh8CHqP9ecRe8e8.e3f'
+name = ''
+key = ''
 project_id = 1
 suite_id = 2
 test_case_ids = {2: 1, 8: 5, 9: 1}
@@ -13,47 +13,38 @@ def test_test_rail_setup():
     url = ''
     name = ''
     key = ''
-    test_rail_functions = TestRailFunctions(url, name, key)
+    test_rail_functions = TestRailFunctions(url, name, key, project_id, suite_id)
     assert test_rail_functions is not None
 
 
 def test_rail_add_run():
-    test_rail_functions = TestRailFunctions(url, name, key)
+    test_rail_functions = TestRailFunctions(url, name, key, project_id, suite_id)
     test_rail_functions.project_id = project_id
-    test_rail_functions.add_new_run(suite_id)
+    test_rail_functions.add_new_run()
 
     assert test_rail_functions.run_id != ''
 
 
 def test_rail_add_case_result():
-    test_rail_functions = TestRailFunctions(url, name, key)
+    test_rail_functions = TestRailFunctions(url, name, key, project_id, suite_id)
     test_rail_functions.project_id = project_id
-    test_rail_functions.add_new_run(2)
+    test_rail_functions.add_new_run()
     test_rail_functions.add_case_result(2, 1, elapsed_time='2')
 
     assert test_rail_functions.result_id != -1
 
 
 def test_rail_add_attachment():
-    test_rail_functions = TestRailFunctions(url, name, key)
+    test_rail_functions = TestRailFunctions(url, name, key, project_id, suite_id)
     test_rail_functions.project_id = project_id
-    test_rail_functions.add_new_run(suite_id)
-    test_rail_functions.add_case_result(2, 1, elapsed_time='2')
-    test_rail_functions.add_attachment_to_result('C:\\globdata.ini')
-
-
-def test_rail_close_run():
-    test_rail_functions = TestRailFunctions(url, name, key)
-    test_rail_functions.project_id = project_id
-    test_rail_functions.add_new_run(suite_id)
+    test_rail_functions.add_new_run()
     test_rail_functions.add_case_result(2, 1, elapsed_time='2')
     test_rail_functions.add_attachment_to_result('C:\\globdata.ini')
 
 
 def test_rail_iterate_cases():
-    test_rail_functions = TestRailFunctions(url, name, key)
-    test_rail_functions.project_id = project_id
-    test_rail_functions.add_new_run(suite_id)
+    test_rail_functions = TestRailFunctions(url, name, key, project_id, suite_id)
+    test_rail_functions.add_new_run()
 
     for case_key in test_case_ids:
         test_rail_functions.add_case_result(case_key, test_case_ids[case_key], elapsed_time='2')
